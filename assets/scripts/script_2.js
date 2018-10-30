9d2e28df6dbb6ccd39652432f522c6c9ef06366b
Εμμανουήλ Μαθιουδάκης <43948050+p17math@users.noreply.github.com>
2018-10-30 18:40:17 +0200

    Update script_2.js

--- assets/scripts/script_2.js
+++ assets/scripts/script_2.js
@@ -87,7 +87,7 @@ d3.json("assets/data/us.json", function(data) {
             .datum(d.parent)
             .select("rect")
             .attr("fill", function () {
-                return '#ffffff'
+                return '#001a33'
             });
         var g1 = svg.insert("g", ".grandparent")
             .datum(d)
